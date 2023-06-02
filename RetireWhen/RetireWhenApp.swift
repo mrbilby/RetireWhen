66422467948b9d8c1591436553090bb9320c9d7f
mrbilby <baileymrb@gmail.com>
2023-06-02 09:35:35 +0100

    Initial Commit

---
+++ RetireWhen/RetireWhenApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetireWhenApp.swift
+//  RetireWhen
+//
+//  Created by James Bailey on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RetireWhenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
